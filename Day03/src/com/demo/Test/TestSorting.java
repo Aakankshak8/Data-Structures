97a7dbf1d24165238dc8db20ae0cbd7099c1e1ed
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-10-31 23:32:10 +0530

    31oct25

---
+++ Day03/src/com/demo/Test/TestSorting.java
@@ -0,0 +1,19 @@
+
+package com.demo.Test;
+
+import java.util.stream.IntStream;
+
+import com.demo.sort.Sorting;
+
+public class TestSorting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[]= {21,2,5,1,7,8,10,3};
+//		Sorting.bubblesort(arr);
+		Sorting.BubbleSortImprvd(arr);
+
+	}
+
+}
---
+++ Day03/src/com/demo/sort/Sorting.java
@@ -0,0 +1,44 @@
+
+package com.demo.sort;
+
+import java.util.Arrays;
+
+public class Sorting {
+	public static void bubblesort(int[] arr) {
+		for(int i=0;i<arr.length; i++) {
+			for(int j=0; j<arr.length-1-i; j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+			System.out.println();
+			System.out.println("Iteration: "+i);
+			System.out.println(Arrays.toString(arr));
+		}
+	}
+	public static void BubbleSortImprvd(int arr[]) {
+		boolean flag ;
+		for(int i=0;i<arr.length;i++) {
+			flag =false;
+			for(int j=0;j<arr.length-1-i;j++) {
+				if(arr[j]>arr[j+1]) {
+					
+				flag = true;
+				
+				int temp = arr[j];
+				arr[j]=arr[j+1];
+				arr[j+1]=temp;
+				}
+			}
+			System.out.println();
+			System.out.println("Iteration: "+i);
+			System.out.println(Arrays.toString(arr));
+			if(!flag) {
+				break;
+			}
+		}
+	}
+
+}

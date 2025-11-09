bf026c7a46781bc53c36a02af7c86bbed60436ba
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-11-09 23:10:40 +0530

    08 NOV 25

---
+++ Day09/src/com/demo/test/TestCircularQueueusingArray.java
@@ -0,0 +1,19 @@
+package com.demo.test;
+
+import com.demo.QueueArray.*;
+
+public class TestCircularQueueusingArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		CircularQueueusingArray ob=new CircularQueueusingArray();
+		ob.dequeue();
+		ob.enqueue(45);
+		ob.enqueue(89);
+		ob.enqueue(39);
+		
+		
+	}
+
+}

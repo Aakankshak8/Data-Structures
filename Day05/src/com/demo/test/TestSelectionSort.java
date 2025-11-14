552aa7253cf9e586046722794b0096d37181294e
Aakankshak8 <karaleaakanksha.7336@gmail.com>
2025-11-14 08:27:42 +0530

    04 NOV 25

---
+++ Day05/src/com/demo/test/TestSelectionSort.java
@@ -0,0 +1,20 @@
+
+package com.demo.test;
+
+import java.util.Scanner;
+
+import com.demo.sorting.SelectionSort;
+
+public class TestSelectionSort {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Size of the array\n");
+		int n = sc.nextInt();
+		int [] arr = new int[n];
+		System.out.println("Enter the elements of the array\n");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		SelectionSort.SelectionSort(arr);
+	}
+}
